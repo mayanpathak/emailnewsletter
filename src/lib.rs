0bc843d1e7707070fbdaa45d0f5136ac13fd0b92
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-14 06:06:27 +0530

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,19 @@
 
 
 
-
+use actix_web::{web, App, HttpResponse, HttpServer};
+async fn health_check() -> HttpResponse {
+HttpResponse::Ok().finish()
+}
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
 pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/hello/{name}", web::get().to(greet))
-            .route("/hello", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+HttpServer::new(|| {
+App::new()
+.route("/health_check", web::get().to(health_check))
+})
+.bind("127.0.0.1:8000")?
+.run()
+.await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("hello {}!", name)
-}
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("world");
+//     format!("hello {}!", name)
+// }
+// async fn health_check(req: HttpRequest) -> impl Responder {
+//     HttpResponse::Ok()
+// }
 
-#[tokio::main] // use tokio runtime
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/hello/{name}", web::get().to(greet))
-            .route("/hello", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+// #[tokio::main] // use tokio runtime
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/hello/{name}", web::get().to(greet))
+//             .route("/hello", web::get().to(greet))
+//             .route("/health_check", web::get().to(health_check))
+//     })
+//     .bind("127.0.0.1:8080")?
+//     .run()
+//     .await
+// }
+
+
+use emailnewsletter :: run;
+
+#[tokio :: main]
+async fn main () -> std :: io :: Result<()> {
+    run().await
 }
